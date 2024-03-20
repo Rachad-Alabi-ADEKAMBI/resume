6d11f80682fa18b2f497271d943201562968398b
Rachad-Alabi-ADEKAMBI <adekambirachad@gmail.com>
2024-03-20 08:57:30 +0100

    update

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# resume
+# Resume
+
+This repository contains my resume. You're welcome to clone it and update the details if you're looking for a web developer resume.
+
+After making your changes, simply print the page as a PDF, and your customized resume is ready.
+
+Best of luck!
---
+++ index.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://kit.fontawesome.com/b14771b76e.js" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="js/script.js"></script>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="content">
+        <div class="content__left">
+            <section class="section about">
+                <h1>
+                    Rachad Alabi ADEKAMBI
+                </h1>
+
+                <span>
+                    Full-stack Developer
+                </span>
+
+                <ul>
+                    <li><i class="fas fa-envelope"></i> adekambirachad@gmail.com <i></i></li>
+                    <li><i class="fas fa-phone"></i> +229 96228860</li>
+                    <li><i class="fas fa-map-pin"></i> Cotonou, Benin republic</li>
+                    <li><i class="fas fa-globe"></i> rachad-alabi-adekambi.github.io/portfolio</li>
+                    <li><i class="fab fa-linkedin"></i> linkedin.com/in/rachad-alabi-adekambi-2753a21b5
+                    </li>
+                </ul>
+            </section>
+            <br>
+
+            <section class="section education">
+                <h2>
+                    EDUCATION
+                </h2>
+                <hr>
+
+                <div class="education__item">
+                    <strong>Computer science course</strong> <br>
+                    <span>Open classrooms</span>
+                    <p> <i class="far fa-calendar"></i> 2020
+                        <i class="fas fa-map-pin"></i> Remote
+                    </p>
+                </div>
+
+            </section>
+
+            <section class="section skills">
+                <h2>SKILLS</h2>
+                <hr>
+
+                <ul>
+                    <li>Laravel</li>
+                    <li>VuesJs</li>
+                    <li>Flask</li>
+                    <li>JavaScript</li>
+                    <li>PHP</li>
+                    <li>MYSQL</li>
+                    <li>Python</li>
+                    <li>HTML</li>
+                    <li>WordPress</li>
+                    <li>CSS</li>
+                    <li>Git</li>
+                    <li>AWS</li>
+
+                </ul>
+            </section>
+
+            <section class="section skills">
+                <h2>SPOKEN LANGUAGES</h2>
+                <hr>
+
+                <ul>
+                    <li>English</li>
+                    <li>French</li>
+                    <li>Spanish</li>
+
+                </ul>
+            </section>
+        </div>
+
+        <div class="content__right">
+
+            <div class="box">
+                <h2>WORK EXPERIENCE</h2>
+                <hr>
+
+                <h3>Full-stack Developer</h3>
+
+                <span>ComeUp</span>
+
+                <div class="details">
+                    <p>
+                        <i class="far fa-calendar"></i>
+                        January 2022 - Nowaday
+                    </p>
+
+                    <p>
+                        <i class="fas fa-map-pin"></i>
+                        Remote
+                    </p>
+                </div>
+
+                <ul>
+                    <li>
+                        Completed over 70 gigs with a 100% customer satisfaction rate
+                    </li>
+                    <li>Developed customized website designs using frameworks
+                        such as Laravel, Vue.js, and Flask
+                    </li>
+                    <li>
+                        Developed custom WordPress extensions and themes
+                    </li>
+                    <li>Transformed Figma and Photoshop mockups into responsive and functional websites</li>
+                    <li>Created custom APIs to deliver secure and fast data</li>
+                    <li>Debugged PHP and JavaScript code</li>
+                </ul>
+            </div> <br>
+
+            <div class="box">
+                <h3>Junior Web Developer</h3>
+
+                <span>Oba Code</span>
+
+                <div class="details">
+                    <p>
+                        <i class="far fa-calendar"></i>
+                        October 2019 - September 2021
+                    </p>
+
+                    <p>
+                        <i class="fas fa-map-pin"></i>
+                        Cotonou, Benin republic
+                    </p>
+                </div>
+
+                <ul>
+                    <li>Used Git for version control to decrease code conflicts </li>
+                    <li>Customized website content with WordPress, improving content management efficiency</li>
+                    <li>Collaborated with designers and performance team on designing updates to sites and blogs,
+                        improving user experience</li>
+                    <li>Briefed development team on website research, and made suggestions for website builds/products
+                    </li>
+                    <li>Participated in stand-ups 2 times a month, where team leaders would review, assign, and coach
+                        team members</li>
+                </ul>
+            </div> <br>
+
+            <div class="box">
+                <h3>Accountant</h3>
+
+                <span>AHVIC</span>
+
+                <div class="details">
+                    <p>
+                        <i class="far fa-calendar"></i>
+                        December 2014 - June 2018
+                    </p>
+
+                    <p>
+                        <i class="fas fa-map-pin"></i>
+                        Cotonou, Benin republic
+                    </p>
+                </div>
+
+                <ul>
+                    <li>Managed financial records and prepared financial statements</li>
+                    <li>Performed budget analysis and cost control measures</li>
+                    <li>Collaborated with auditors during financial audits</li>
+                    <li>Provided financial insights and recommendations to management</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
